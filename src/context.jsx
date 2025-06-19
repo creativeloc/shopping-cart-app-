ed07660eba277397a4519b2d9a097814913d9272
creativeloc <creativeloc@yahoo.com>
2025-06-19 06:53:42 -0400

    defined the useRducer hook and created a separate reducer.js file

--- src/context.jsx
+++ src/context.jsx
@@ -1,12 +1,25 @@
 import { useContext, useReducer, useEffect, createContext } from "react"
+import reducer from "./reducer"
 
 const AppContext = createContext()
 
+// NOTE: the 'useReducer hook' accepts two parameters ('reducer' and the 'initialState')
+
+// 1st.parameter: reducer
+
+// 2nd. parameter: initialState
+const initialState = {
+  loading: false,
+  cart: []
+}
+
 export const AppProvider = ({ children }) => {
-  const greeting = "Hello, World!"
+  // Just like in 'useState', the useReducer hook is also
+  //  destructured to produce 'state' and 'action' items
+  const [state, dispatch] = useReducer((reducer, initialState))
 
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   )
 }
 
