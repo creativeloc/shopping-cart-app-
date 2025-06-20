253681ab6c900e595e23f44acbfefc4f96f69222
creativeloc <creativeloc@yahoo.com>
2025-06-20 05:54:39 -0400

    complete useReducer setup by defing the reducer and action items

--- src/context.jsx
+++ src/context.jsx
@@ -1,21 +1,35 @@
 import { useContext, useReducer, useEffect, createContext } from "react"
 import reducer from "./reducer"
 
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS
+} from "./actions"
+
 const AppContext = createContext()
 
-// NOTE: the 'useReducer hook' accepts two parameters ('reducer' and the 'initialState')
+/*
+  reducer: 1st.parameter
+    const reducer = (state, action) => {
+    return state
+  }
+  // put this in a separate file (reducer.js) and import it into this file
+*/
 
-// 1st.parameter: reducer
-
-// 2nd. parameter: initialState
+// initialState: 2nd. parameter
 const initialState = {
   loading: false,
   cart: []
 }
 
 export const AppProvider = ({ children }) => {
+  // NOTE: 'useReducer hook' accepts two parameters ('reducer' and the 'initialState')
   // Just like in 'useState', the useReducer hook is also
-  //  destructured to produce 'state' and 'action' items
+  //  Destructured to produce 'state' and 'action' items
   const [state, dispatch] = useReducer((reducer, initialState))
 
   return (
@@ -26,3 +40,4 @@ export const AppProvider = ({ children }) => {
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
+//
